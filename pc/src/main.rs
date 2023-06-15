ba04385aeaa49ecaea7d38ead1e973033a73166e
Iamhere345 <lunarome08@icloud.com>
2023-06-15 18:16:58 +1000

    add UDP code to main.py (SenseHAT code commented out due to emulation problems, see comment at the top of main.py), realise that you can only send binary strings over UDP (duh), so now i'm encoding floating point values for the gyro data (which has one decimal point, so it has to be at least f32) as string to send over UDP. Still TODO: decode packets in main.rs - for now i'm just printing the whole packet as a utf8-encoded string.

--- pc/src/main.rs
+++ pc/src/main.rs
@@ -1,21 +1,18 @@
 use std::net::UdpSocket;
 
+// TODO
 /*
 
-TODO
 conf.toml file for story stuff like the raspberry pi's ip addresss and port
 
 */
 
+
 /*
 
 Packet Architecture
-Len: 4
-Data:
-1 - constant known by both the client and the server to ensure either device is talking to the correct device
-2 - pitch
-3 - roll
-4 - yaw
+TODO
+
 
 */
 
@@ -37,36 +34,35 @@ fn to_mouse_movement(pitch: f32, roll: f32, yaw: f32) -> (f32, f32) {
 }
 
 // TODO
-fn move_mouse(move_by: (f32, f32)) -> Option<_> {
-    None
+fn move_mouse(move_by: (f32, f32)) {
+
 }
 
 fn main() {
     
     let socket = UdpSocket::bind(format!("{}:{}", RPI_IP, RPI_PORT)).expect("Unable to bind socket to listening port");
 
-    let mut buf: [f32; 4] = [0.0; 4];
+    let mut buf = [0; 1024];
 
     loop {
     
         let result_res = socket.recv(&mut buf);
 
         if result_res.is_err() {
-            println!("failed to recv packet");
+            println!("failed to recv packet. Error: {}", result_res.unwrap_err());
             continue;
         }
 
         let result = result_res.unwrap();
 
-        if result != PACKET_LEN {
-            println!("unexpected packen len");
-        }
-
         // get data part of packet (pitch roll and yaw)
-        let buf = &mut buf[1..3];
+        // TODO parse buffer to get data
+        println!("{:?}", String::from_utf8_lossy(&buf));
 
+        /*
         let (x, y) = to_mouse_movement(buf[0], buf[1], buf[2]);
         move_mouse((x, y));
+        */
 
     }
 
--- rpi/main.py
+++ rpi/main.py
@@ -1,17 +1,39 @@
-from sense_hat import SenseHat
+# from sense_emu import SenseHat
 import time
+import socket
+
+#! i couldn't get sense_emu working on windows. I might have another go on MacOS, but for now i may have to leave the proper testing
+#! until i boot up the raspberry pi again. I've temporarily commented out SenseHAT-related code, and instead of emulator gryo data
+#! i'm using constant test values
 
 UPDATE_INTERVAL = 0.5
 
-sense = SenseHat()
-sense.clear()
+UDP_IP = "127.0.0.1"
+UDP_PORT = 8080
+
+# SenseHAT init
+# sense = SenseHat()
+# sense.clear()
+
+# UDP init
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while True:
   
-  o = sense.get_orientation()
-  pitch = o["pitch"]
-  roll = o["roll"]
-  yaw = o["yaw"]
-  
+  # o = sense.get_orientation()
+  # pitch = o["pitch"]
+  # roll = o["roll"]
+  # yaw = o["yaw"]
+
+  # test values
+  pitch = 86.4
+  roll = 2.3
+  yaw = 22.9
+
   print("pitch {0} roll {1} yaw {2}".format(pitch, roll, yaw))
+
+  # encoding floating point values as an ascii string...
+  sendData = (str(pitch) + "_" + str(roll) + "_" + str(yaw)).encode('ascii')
+  sock.sendto(sendData, (UDP_IP, UDP_PORT))
+
   time.sleep(UPDATE_INTERVAL)
