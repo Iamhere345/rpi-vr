3ff7891193ba0f83b2e22b5de9783f1a61ff82b2
Iamhere345 <lunarome08@icloud.com>
2023-06-17 15:11:56 +1000

    decode packet data

--- pc/src/main.rs
+++ pc/src/main.rs
@@ -39,25 +39,73 @@ fn move_mouse(move_by: (f32, f32)) {
 }
 
 fn main() {
-    
+
     let socket = UdpSocket::bind(format!("{}:{}", RPI_IP, RPI_PORT)).expect("Unable to bind socket to listening port");
 
     let mut buf = [0; 1024];
 
-    loop {
+    'main: loop {
     
         let result_res = socket.recv(&mut buf);
 
         if result_res.is_err() {
-            println!("failed to recv packet. Error: {}", result_res.unwrap_err());
+            println!("[ERROR] Failed to recv packet. Error: {}", result_res.unwrap_err());
             continue;
         }
 
-        let result = result_res.unwrap();
-
         // get data part of packet (pitch roll and yaw)
-        // TODO parse buffer to get data
-        println!("{:?}", String::from_utf8_lossy(&buf));
+        let data_raw = String::from_utf8_lossy(&buf).to_string();
+
+        println!("{}", data_raw);
+
+        // these shouldn't need to be mut but apparently rust hates me
+        let (mut pitch, mut roll, mut yaw): (f64, f64, f64) = (0.0, 0.0, 0.0);
+
+        let data_split = data_raw.splitn(3, '_');
+
+        // there cannot be less than 3 values
+        if data_split.clone().collect::<Vec<_>>().len() < 3 {
+            println!("[ERROR] Invalid packet data (less than 3 values received)")
+        }
+
+        for (i, _axis) in data_split.enumerate() {
+
+            let mut axis = _axis.clone();
+
+            // last value contains \0's that make up the rest of the buffer
+            // these need to be removed so the value can be parsed
+            if i == 2 {
+                let axis_trim = axis.split_once('\0');
+                axis = match axis_trim {
+                    Some(a) => a.0,
+                    None => {
+                        println!("[WARNING] Buffer full.");
+                        axis
+                    }
+                };
+            }
+
+            let axis_parse_result = axis.parse::<f64>();
+
+            let axis_float = match axis_parse_result {
+                Ok(f) => f,
+                Err(e) => {
+                    println!("{:?}", axis.as_bytes());
+                    println!("[ERROR] Invalid packet (unable to decode data). Error: {}", e);
+                    continue 'main;
+                }
+            };
+
+            match i {
+                0 => pitch = axis_float,
+                1 => roll = axis_float,
+                2 => yaw = axis_float,
+                _ => {/* this shouldn't happen because splitn will never return more than 3 values */}
+            }
+
+        }
+
+        println!("pitch: {} roll: {} yaw: {}", pitch, roll, yaw);
 
         /*
         let (x, y) = to_mouse_movement(buf[0], buf[1], buf[2]);
